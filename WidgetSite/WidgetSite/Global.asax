f0eb2eaf532bf65a84d7a8bb7c521f10da067781
glav <glav@theglavs.com>
2016-05-30 13:12:01 +1000

    Just beginning to create the initial site and associated components with some basic dependencies

---
+++ WidgetSite/WidgetSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WidgetSite.MvcApplication" Language="C#" %>
---
+++ WidgetSite/WidgetSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WidgetSite.Startup))]
+namespace WidgetSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
